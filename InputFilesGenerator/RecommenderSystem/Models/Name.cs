3c3c53c766b7e1f5f8adde0788dc733c65a8f514
Flavius Topan(uidj1061) <flavius.topan@continental-corporation.com>
2021-05-17 14:16:00 +0300

    input files gen added

---
+++ InputFilesGenerator/RecommenderSystem/Models/Name.cs
@@ -0,0 +1,29 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecommenderSystem.Models
+{
+    public class Name
+    {
+        public string nconst { get; set; }
+        public string primaryName { get; set; }
+        public string birthYear { get; set; }
+        public string deathYear { get; set; }
+        public string primaryProfession { get; set; }
+        public string knownForTitles { get; set; }
+    }
+
+    public class NameShort
+    {
+        public string nconst { get; set; }
+
+        public string primaryName { get; set; }
+    }
+}
---
+++ InputFilesGenerator/RecommenderSystem/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecommenderSystem.Models
+{
+    public class User
+    {
+        public string userId { get; set; }
+        public string age { get; set; }
+        public string gender { get; set; }
+        public string occupation { get; set; }
+    }
+
+    public class UserRating
+    {
+        public string userId { get; set; }
+
+        public string movieId { get; set; }
+
+        public string userRating { get; set; }
+    }
+}
